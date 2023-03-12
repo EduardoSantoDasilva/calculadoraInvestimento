5e03c40933aa3296462a25cea08acdd3dd1f7f80
EDUARDO-SANTOS <eduardosantospx@gmail.com>
2023-03-12 18:28:53 -0300

    teste button click

--- script.js
+++ script.js
@@ -1,104 +1,3 @@
-const options = {
-  series: [
-    {
-      name: "cambio",
-      data: [
-        {
-          x: new Date("2018-02-12").getTime(),
-          y: 5.18,
-        },
-        {
-          x: new Date("2018-02-13").getTime(),
-          y: 5.3,
-        },
-        {
-          x: new Date("2018-02-14").getTime(),
-          y: 5.18,
-        },
-        {
-          x: new Date("2018-02-15").getTime(),
-          y: 5.11,
-        },
-        {
-          x: new Date("2018-02-16").getTime(),
-          y: 5.18,
-        },
-        {
-          x: new Date("2018-02-17").getTime(),
-          y: 5.25,
-        },
-        {
-          x: new Date("2018-02-18").getTime(),
-          y: 5.18,
-        },
-        {
-          x: new Date("2018-02-19").getTime(),
-          y: 5.2,
-        },
-      ],
-    },
-  ],
-  chart: {
-    height: 350,
-    type: "area",
-    toolbar: {
-      show: false,
-    },
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  stroke: {
-    curve: "straight",
-  },
-  yaxis: {
-    min: 5,
-    tickAmount: 4,
-    labels: {
-      formatter: (value) => {
-        return value.toFixed(1).replace('.', ',')
-      },
-    },
-  },
-  xaxis: {
-    labels: {
-      show: false,
-    },
-    tooltip: {
-      enabled: false,
-    },
-    axisTicks: {
-      show: false,
-    },
-  },
-  fill: {
-    gradient: {
-      shadeIntensity: 1,
-      opacityFrom: 0.7,
-      opacityTo: 0.9,
-      stops: [0, 90, 100],
-    },
-  },
-  colors: ["#7C3AED"],
-  tooltip: {
-    custom: function ({ series, seriesIndex, dataPointIndex, w }) {
-      return `<div class="tooltip">
-    <span>${String(series[seriesIndex][dataPointIndex]).replace('.', ',')}</span>
-    <span>${new Date(
-      w.globals.seriesX[seriesIndex][dataPointIndex]
-    ).toLocaleDateString("pt-BR", {
-      weekday: "short",
-      month: "short",
-      day: "numeric",
-    })}</span>
-    </div>`
-    },
-  },
-}
-
-const chart = new ApexCharts(document.querySelector("#chart"), options)
-chart.render()
-
 function getFinance(){
 
   const result  = fetch("https://api.hgbrasil.com/finance/quotations?format=json-cors&key=2fbecdab")
@@ -111,7 +10,7 @@ function getFinance(){
 }
 
 
-let middle = document.querySelector(".middle")
+let middle = document.querySelector(".middle");
 
 middle.addEventListener("click", async()=>{
 
@@ -119,10 +18,10 @@ middle.addEventListener("click", async()=>{
   console.log(`${JSON.stringify(result.USD.buy, undefined,2)}`);
   let valor = `${JSON.stringify(result.USD.buy, undefined,2)}`
   console.log("clicou");
-  let usd = document.querySelector(".amount").value;
-  let real = document.querySelector(".amounte").value;
+  let real = document.querySelector("#real").value;
 
   real.value = real * parseFloat(valor);
+  console.log(real.value);
 
 
 })
